71f8f3db82e42b9ebfa45ed24da0a09daddafe8e
Vigneshkumar19112001 <113460160+Vigneshkumar19112001@users.noreply.github.com>
2026-03-24 15:20:49 +0000

    First

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            # Broadcast to all clients
+            for client in clients:
+                await client.send_text(data)
+
+    except WebSocketDisconnect:
+        clients.remove(websocket)
